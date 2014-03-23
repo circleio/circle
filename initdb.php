eef4f8e7543eb49650fd96a0337b07d314f4548e
Sambuddha Basu <sammygamer@live.com>
2014-03-23 20:31:27 +0530

    login page improved

--- initdb.php
+++ initdb.php
@@ -6,8 +6,6 @@
         mysqli_query($connect, $create_db_query) or die(mysqli_error());
     }
     mysqli_select_db($connect, $dbname);
-    $hello = mysqli_query($connect, 'SHOW TABLES;');
-    print_r($hello);
     $users_table = "CREATE TABLE IF NOT EXISTS users(
                        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                        first_name VARCHAR(30) NOT NULL,
@@ -20,6 +18,6 @@
                        fb_account VARCHAR(30) DEFAULT null,
                        twitter_account VARCHAR(30) DEFAULT null);";
     mysqli_query($connect, $users_table) or die("cannot create table");
-    $add_admin = "INSERT INTO users VALUES(1, 'admin', 'admin', 'admin@example.com', 'admin', 'admin', 1, 0, null, null);";
+    $add_admin = "INSERT INTO users VALUES(1, 'admin', 'admin', 'admin@example.com', 'admin', md5('admin'), 1, 0, null, null);";
     mysqli_query($connect, $add_admin) or die("cannot add admin");
 ?>
--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 include 'includes.php';
 include 'header.php';
 ?>
+<script src="static/js/login.js"></script>
 <div id="wrap">
     <div id="main" class="container clear-top">
         <div class="row">
@@ -17,15 +18,15 @@ include 'header.php';
         <div class="row">
             <div class="col-lg-3"></div>
             <div class="col-lg-6">
-                <form class="form-horizontal">
+                <div class="form-horizontal">
                     <fieldset>
-                        <div class="form-group">
+                        <div class="form-group" id="input_username">
                             <label for="input_username" class="col-lg-3 control-label">Username</label>
                             <div class="col-lg-9">
                                 <input type="text" class="form-control" id="username" placeholder="Username">
                             </div>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group" id="input_password">
                             <label for="input_password" class="col-lg-3 control-label">Password</label>
                             <div class="col-lg-9">
                                 <input type="password" class="form-control" id="password" placeholder="Password">
@@ -33,11 +34,11 @@ include 'header.php';
                         </div>
                         <div class="form-group">
                             <div class="col-lg-9 col-lg-offset-3">
-                                <button type="submit" class="btn btn-primary">LogIn</button>
+                                <button type="submit" class="btn btn-primary" onClick="login();">LogIn</button>
                             </div>
                        </div>
                    </fieldset>
-                </form>
+                </div>
             </div>
             <div class="col-lg-3"></div>
         </div>
