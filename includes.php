efa09385754c6729a72ebea2538d61fdc83ce65d
amoghbl1 <amoghbl1@gmail.com>
2014-03-23 19:52:47 +0530

    changed header links

--- includes.php
+++ includes.php
@@ -1,5 +1,5 @@
 <?php
-    $BASE_URL = 'http://' . $_SERVER[HTTP_HOST];
+    $BASE_URL = 'http://' . $_SERVER[HTTP_HOST] . dirname($_SERVER['PHP_SELF']);
 ?>
 <link rel="stylesheet" href="static/css/bootstrap.css" type="text/css">
 <link rel="stylesheet" href="static/css/bootstrap.min.css" type="text/css">
