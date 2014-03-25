d13caf533eb0a642c8d0bebd300a2505f94e961b
amoghbl1 <amoghbl1@gmail.com>
2014-03-25 20:01:02 +0530

    minor changes in ask.php

--- ask.php
+++ ask.php
@@ -9,7 +9,7 @@ include 'header.php';
 ?>
 <script src="http://www.google.com/jsapi" type="text/javascript"></script>
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="static/js/getFeed.js"></script>
+<script src="static/js/getAskFeed.js"></script>
 <script>
 var obj = new Object();
 window.onload = getAllUrlParameters(obj);
