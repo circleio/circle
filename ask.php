6cefd30008e92afc23e4e187dd1546eb37117b62
amoghbl1 <amoghbl1@gmail.com>
2014-03-26 22:13:58 +0530

    ask.php check for login before loading

--- ask.php
+++ ask.php
@@ -2,7 +2,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['username'])) {
-        //header('Location: index.php');
+        header('Location: index.php');
 }
 include 'includes.php';
 include 'header.php';
