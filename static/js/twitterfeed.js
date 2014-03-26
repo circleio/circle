e6c66ba95ee2a2559276b7b0b9f52ea0de6d3919
Sambuddha Basu <sammygamer@live.com>
2014-03-26 22:51:07 +0530

    Checks retweets

--- static/js/twitterfeed.js
+++ static/js/twitterfeed.js
@@ -8,10 +8,26 @@ xmlhttp.onreadystatechange=function() {
         var response = JSON.parse(xmlhttp.responseText);
         var html_response = '';
 	for(var i=0;i<response.data.length;i++) {
-            html_response += '<div class="row"><div class="col-lg-1"><img src="static/images/twitter.png" style="margin-top: 20px; width: 50px;"></div><div class="col-lg-10"><div class="row"><div class="col-lg-12"><span><a style="color: #fff; font-size: 3em;" href="https://twitter.com/' + response.data[i].screen_name + '">'+ response.data[i].name + '</a></span></div></div><div class="row"><div class="col-lg-12"><span style="font-size: 1.25em;" class="text-primary">' + response.data[i].text + '</span></div></div></div><div class="col-lg-1"><div class="row"><div class="col-lg-12"><a href="api/twitter/retweet.php?id=' + response.data[i].id + '"><img src="static/images/twitter_retweet.jpg" style="margin-top: 20px; width: 50px;"></a></div></div></div></div><hr>'
+            html_response += '<div class="row"><div class="col-lg-1"><img src="static/images/twitter.png" style="margin-top: 20px; width: 50px;"></div><div class="col-lg-10"><div class="row"><div class="col-lg-12"><span><a style="color: #fff; font-size: 3em;" href="https://twitter.com/' + response.data[i].screen_name + '">'+ response.data[i].name + '</a></span></div></div><div class="row"><div class="col-lg-12"><span style="font-size: 1.25em;" class="text-primary">' + response.data[i].text + '</span></div></div></div><div class="col-lg-1">';
+            if(!response.data[i].retweeted) {
+                html_response += '<div class="row"><div class="col-lg-12"><img id="' + response.data[i].id + '"onClick="retweet_id(\'' + response.data[i].id + '\');" src="static/images/twitter_retweet.jpg" style="margin-top: 20px; width: 50px;"></div></div>';
+	    }
+	    html_response += '</div></div><hr>';
 	}
         $('#main').append(html_response);
     }
 }
 xmlhttp.open("GET", twitter_url + '?username=sambuddhabasu', true);
 xmlhttp.send();
+function retweet_id(tweet_id) {
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function() {
+        if(xmlhttp.readyState==4 && xmlhttp.status==200) {
+            var image = document.getElementById(tweet_id);
+            image.style.display="none";
+	}
+    }
+    var url = 'api/twitter/retweet.php?id=' + tweet_id;
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
