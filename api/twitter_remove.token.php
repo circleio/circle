e6277025406632b105e23ef209b02b22e466af96
Sambuddha Basu <sammygamer@live.com>
2014-03-27 21:41:26 +0530

    Remove twitter token

---
+++ api/twitter_remove.token.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+include '../dbconnect.php';
+mysqli_select_db($connect, $dbname);
+$query = "UPDATE users SET twitter_token=null, twitter_token_secret=null WHERE id='" . $_SESSION['id'] . "';";
+mysqli_query($connect, $query);
+?>
--- static/js/settings.js
+++ static/js/settings.js
@@ -6,10 +6,20 @@ xmlhttp.onreadystatechange=function() {
             var html_response = '<div class="row"></div class="col-lg-12"><a href="api/twitter_get_token.php"><button class="btn btn-primary" type="button">Authorize Account</button></a></div></div>';
 	}
         else {
-            var html_response = '<div class="row"></div class="col-lg-12"><button class="btn btn-primary" type="button">Unauthorize Account</button></div></div>';
+            var html_response = '<div class="row"></div class="col-lg-12"><button class="btn btn-primary" onClick="disallow();" type="button">Unauthorize Account</button></div></div>';
 	}
         $('#twitter_account').append(html_response);
     }
 }
 xmlhttp.open("GET", "api/twitter_check_token.php", true);
 xmlhttp.send();
+function disallow() {
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function() {
+        if(xmlhttp.readyState==4 && xmlhttp.status==200) {
+            location.reload();
+	}
+    }
+    xmlhttp.open("GET", "api/twitter_remove_token.php", true);
+    xmlhttp.send();
+}
