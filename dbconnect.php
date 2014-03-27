23f3fb708e67b99ef0b8f9b87c51a6a290272067
amoghbl1 <amoghbl1@gmail.com>
2014-03-27 11:26:01 +0530

    remove debugging...

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 //debugging!
-ini_set('display_errors',1);
-error_reporting(E_ALL);
+//ini_set('display_errors',1);
+//error_reporting(E_ALL);
     $dbname = "circle";
     $dbserver = "localhost";
     $dbuser = "root";
