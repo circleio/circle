d19ba781ca0d76fccbfedad3e858174d7bcae4a9
Sambuddha Basu <sammygamer@live.com>
2014-03-27 21:59:14 +0530

    Fixed twitter denied

--- api/twitter_get_token_callback.php
+++ api/twitter_get_token_callback.php
@@ -5,10 +5,14 @@ define("CONSUMER_KEY", "UBHtHJkGEGwnbKPvjrsLA");
 define("CONSUMER_SECRET", "apP8rNPgf6D86iRlRQ6HWXdSWiRldlFUnJikm9MIM8");
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
 $access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
-$oauth_token = $access_token['oauth_token'];
-$oauth_token_secret = $access_token['oauth_token_secret'];
-include '../dbconnect.php';
-mysqli_select_db($connect, $dbname) or die("cannot connect to database");
-$query = "UPDATE users SET twitter_token='" . $oauth_token . "', twitter_token_secret='" . $oauth_token_secret . "' WHERE id='" . $_SESSION['id'] . "'";
-mysqli_query($connect, $query);
+if(isset($access_token['oauth_token'])) {
+    $oauth_token = $access_token['oauth_token'];
+    $oauth_token_secret = $access_token['oauth_token_secret'];
+    include '../dbconnect.php';
+    mysqli_select_db($connect, $dbname) or die("cannot connect to database");
+    $query = "UPDATE users SET twitter_token='" . $oauth_token . "', twitter_token_secret='" . $oauth_token_secret . "' WHERE id='" . $_SESSION['id'] . "'";
+    mysqli_query($connect, $query);
+}
+include '../includes.php';
+header('Location: ' . $BASE_URL);
 ?>
