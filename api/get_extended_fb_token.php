39586e5bd14a245465ff20c0dd7e0bc8938d9fa8
mishraiit <ayushmishra.iit@gmail.com>
2014-03-28 09:38:26 +0000

    .

---
+++ api/get_extended_fb_token.php
@@ -0,0 +1,12 @@
+<?php
+	$access_token = trim($_REQUEST[access_token]);
+	$appId = '420288881450005';
+	$appSecret = '39dd9707bb75b8bb0f8b54ed689eaea3';
+
+	$url = "https://graph.facebook.com/oauth/access_token?client_id=$appId&client_secret=$appSecret&grant_type=fb_exchange_token&fb_exchange_token=$access_token";
+
+	$result = file_get_contents($url);
+
+	echo $result;
+
+?>
