646f777bed15329cfe6c8d9c2c84fa6ceae2504b
mishraiit <ayushmishra.iit@gmail.com>
2014-03-28 12:11:42 +0000

    .

--- api/facebook_get_feeds.php
+++ api/facebook_get_feeds.php
@@ -9,10 +9,12 @@ mysqli_select_db($connect, $dbname);
 
 $query = "SELECT fb_account FROM users WHERE username = \"$_SESSION[username]\";";
 $result = mysqli_query($connect, $query);
+$count = 25;
+if(isset($_REQUEST['count'])) {
+	$count = $_REQUEST['count'];
+}
 
 $result = mysqli_fetch_assoc($result);
 $result = $result[fb_account];
-echo "Hrl";
-echo $result;
-
+echo file_get_contents("https://graph.facebook.com/me/home?access_token=".$result."&limit=".$count);
 ?>
