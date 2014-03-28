f6d4ccef052c3144155788a2e1322591acb5347e
mishraiit <ayushmishra.iit@gmail.com>
2014-03-28 11:42:35 +0000

    .

---
+++ api/facebook_get_feeds.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])) {
+    header('Location: index.php');
+
+include '../dbconnect.php';
+
+mysqli_select_db($connect, $dbname);
+
+$query = "SELECT fb_account FROM users WHERE username = \"$_SESSION[username]\";";
+$result = mysqli_query($connect, $query);
+
+$result = mysqli_fetch_assoc($result);
+$result = $result[access_token];
+
+echo $result;
+
+?>
