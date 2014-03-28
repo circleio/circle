d1a83ba19c0d7f0fb44fb1d2e28eaa0f86a63fc8
Sambuddha Basu <sammygamer@live.com>
2014-03-29 01:36:32 +0530

    Feeds shown

--- static/js/facebook_feed.js
+++ static/js/facebook_feed.js
@@ -6,8 +6,12 @@ xmlhttp=new XMLHttpRequest();
 xmlhttp.onreadystatechange=function() {
     if (xmlhttp.readyState==4 && xmlhttp.status==200) {
         var response = JSON.parse(xmlhttp.responseText);
-	console.log(response);
+        html_response = '';
+	for(var i=0; i<response.data.length; i++) {
+            html_response += '<div class="row"><div class="col-lg-1"><a href="https://facebook.com/"><img src="static/images/facebook.png" style="margin-top: 20px; margin-left: 35px; width: 30px;"></a></div><div class="col-lg-10"><div class="row"><div class="col-lg-12"><h4>' + response.data[i].message + '</h4></div></div></div><div class="col-lg-1"></div></div>';
+	}
     }
+    $('#main').append(html_response);
 }
 xmlhttp.open("GET", facebook_url, true);
 xmlhttp.send();
