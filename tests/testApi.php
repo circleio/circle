fc8fddce1301049b414f45de79da7cbc5ea6f37b
Sambuddha Basu <sammygamer@live.com>
2014-04-02 03:01:41 +0530

    Added acount activation tests

--- tests/testApi.php
+++ tests/testApi.php
@@ -18,5 +18,16 @@ class testApi extends PHPUnit_Framework_TestCase
         $this->assertEquals('admin', $response->fields->first_name);
         $this->assertEquals('admin', $response->fields->username);
     }
+    public function testInvalidAccountActivation() {
+        $response = exec('curl http://54.203.112.16/circle/api/activate_account.php');
+        $response = json_decode($response);
+        $this->assertEquals(0, $response->status, 'Response 0 should indicate invalid code.');
+    }
+    public function testAccountAlreadyActivated() {
+        $response = exec('curl http://54.203.112.16/circle/api/activate_account.php?code=' . md5('admin@example.com'));
+        $response = json_decode($response);
+        $this->assertEquals(1, $response->status, 'Response 1 should indicate correct code.');
+        $this->assertEquals(0, $response->success, 'Response 0 should indicate that account is already activated.');
+    }
 }
 ?>
