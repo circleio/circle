cfb3b5078edeb702681512c5825679740b00657d
mishraiiit <mishraiiit@vm-0.mishraiiit.koding.kd.io>
2014-04-15 22:18:05 +0000

    .

--- static/js/facebook_feed.js
+++ static/js/facebook_feed.js
@@ -12,11 +12,11 @@ xmlhttp.onreadystatechange=function() {
 	for(var i=0; i<response.data.length; i++) {
             if(response.data[i].picture) response.data[i].picture = response.data[i].picture.replace(/_s/, '_n');
 	    var shares = (response.data[i].shares)?("<strong> Shares </strong> &nbsp&nbsp&nbsp&nbsp" +response.data[i].shares.count):"";
-	    var likes = (response.data[i].likes)?(" <strong> Likes </strong> &nbsp&nbsp&nbsp&nbsp" +response.data[i].likes.data.length):"";
+	    var likes = (response.data[i].likes)?(" <img src=\"https://cdn2.iconfinder.com/data/icons/business-icons-2-3/256/Best_Choice-32.png\" /> &nbsp&nbsp&nbsp" +response.data[i].likes.data.length):"";
 	    var page_link = 'https://facebook.com/'+response.data[i].from.id;
 	    var link = (response.data[i].actions == undefined)?(response.data[i].link):(response.data[i].actions[0].link);
 	    if(response.data[i].to) {
-			response.data[i].from.name = response.data[i].from.name + " ->> " + response.data[i].to.data[0].name;
+			response.data[i].from.name = response.data[i].from.name + " <img src=\"https://cdn2.iconfinder.com/data/icons/snipicons/5000/arrow-right-128.png\" width=\"40px\" height=\"40px\" /> " + response.data[i].to.data[0].name;
 
 	}
 	
@@ -34,8 +34,7 @@ xmlhttp.onreadystatechange=function() {
 		if(response.data[i].caption) response.data[i].message = response.data[i].caption;
 		if(response.data[i].description) response.data[i].message += "<br>" + response.data[i].description;
 	    }
-
-            html_response += '<div class="row"> 	<div class="col-lg-1"> 		<a href="https://facebook.com/"> <img src="static/images/facebook.png" style="margin-top: 20px; margin-left: 35px; width: 30px;"></a> </div> <div class="col-lg-10"> <div class="row"> 	 <a href=\'' + page_link + '\' > <h4>' + response.data[i].from.name  +  '</h4> </a> </div> <div class="row"> <div class="col-lg-12"> 	<h5> <a href=\''+ link  + '\' >' + response.data[i].message + ' </a> </h5> <div class="row">' + likes + '</div> <div class="row">'+ shares + ' </div> </div> </div> </div> <div class="col-lg-1"> </div> </div> <hr>';
+	html_response += '<div class="row">  <div class="col-lg-1"> <a href="https://facebook.com/"> <img src="static/images/facebook.png" style="margin-top: 20px; margin-left: 35px; width: 30px;"></a> </div> <div class="col-lg-10"> <div class="row">   <a href=\'' + page_link + '\' > <h4>' + response.data[i].from.name  +  '</h4> </a> </div> <div class="row"> <div class="col-lg-12">    <h5> <a href=\''+ link  + '\' >' + response.data[i].message + ' </a> </h5> <div class="row"> <table> <td> ' + likes + ' </td>  </table> </div> </div> </div> </div> <div class="col-lg-1"> </div> </div> <hr>';
 	}
         $('#main').append(html_response);
     }
