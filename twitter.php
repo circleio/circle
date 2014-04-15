ddcf8bca8ce8fd69f83a7fd7b875474f5ffb950d
sambuddhabasu <sambuddhabasu@vm-2.sambuddhabasu.koding.kd.io>
2014-04-15 22:41:29 +0000

    Added share for twitter

--- twitter.php
+++ twitter.php
@@ -8,6 +8,14 @@ include 'header.php';
 ?>
 <title>Circle</title>
 <div id="wrap">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=420288881450005";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
     <div id="main" class="container clear-top">
     </div>
 </div>
